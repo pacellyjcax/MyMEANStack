9694fd0fbec2bb1f71cc253561f11d848da2e325
Pacelly J. C. A. Xavier <pacellyjose@gmail.com>
2017-04-08 00:57:10 -0300

    Travis CI Inclusion

--- server/configurations/config.js
+++ server/configurations/config.js
@@ -6,7 +6,7 @@
 
 exports.Mongo = {
   // mongodb configurations
-  uri:'mongodb://localhost/OficinaServerSideMean'
+  uri = 'mongodb://localhost/MyMeanStack';
 };
 
 exports.MySQL = {
@@ -15,5 +15,5 @@ exports.MySQL = {
 exports.Server = {
   //'DEFAULT' DATA FOR THE SERVER
   address: '127.0.0.1',
-  port: 3000
+  var port: 3000
 };
